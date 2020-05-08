b97533ab22e68864ca9e5b8e76c0d5852ef51b7c
Ilya Asha <ilyaasha24@gmail.com>
2020-05-08 17:51:17 +0700

    Update split.c

--- split.c
+++ split.c
@@ -1,53 +1,76 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
-char buf[512];
+char*
+getline(int fd, char *buf, int max)
+{
+  int i, cc;
+  char c;
+
+  for(i = 0; i + 1 < max;) {
+    cc = read(fd, &c, 1);
+    if(cc < 1)
+      break;
+    buf[i++] = c;
+    if(c == '\n' || c == '\r')
+      break;
+  }
+  buf[i++] = '\0';
+  return buf;
+}
 
 void
-wc(int fd, char *name)
+split(int fd, char *file)
 {
-  int i, n;
-  int l, w, c, inword;
-
-  l = w = c = 0;
-  inword = 0;
-  while((n = read(fd, buf, sizeof(buf))) > 0){
-    for(i=0; i<n; i++){
-      c++;
-      if(buf[i] == '\n')
-        l++;
-      if(strchr(" \r\t\n\v", buf[i]))
-        inword = 0;
-      else if(!inword){
-        w++;
-        inword = 1;
-      }
+  char buf[512], name[3] = "xaa";
+  int l, fdt;
+  
+  do {
+    getline(fd, buf, sizeof(buf));
+    l = 0;
+    if((fdt = open(name, O_CREATE | O_RDWR)) < 0) {
+      printf(2, "split: cannot open %s\n", name);
+      exit();
     }
-  }
-  if(n < 0){
-    printf(1, "wc: read error\n");
-    exit();
-  }
-  printf(1, "%d %d %d %s\n", l, w, c, name);
+    while (buf[0] != '\0' && l < 10) {
+      // printf(1, "%d %s\n", l, buf);
+      write(fdt, buf, sizeof(buf));
+      l++;
+      getline(fd, buf, sizeof(buf));
+      // if ((l &= 1 && name[3]/2 == 0) || (l &= 0 && name[3]/2 == 1))
+        // getline(fd, buf, sizeof(buf));
+    }
+    close(fdt);
+
+    if (name[2] >= 'a' && name[2] <= 'z') {
+      name[2]++;
+    } else {
+      name[1]++;
+      name[2] = 'a';
+    }
+
+    // printf(1, "---------------%s---------------\n", name);
+  } while (buf[0] != '\0');
+  
 }
 
 int
 main(int argc, char *argv[])
 {
   int fd, i;
-
-  if(argc <= 1){
-    wc(0, "");
+  if(argc <= 1) {
+    // split(0, "");
     exit();
   }
 
-  for(i = 1; i < argc; i++){
-    if((fd = open(argv[i], 0)) < 0){
-      printf(1, "wc: cannot open %s\n", argv[i]);
+  for(i = 1; i < argc; i++) {
+    if((fd = open(argv[i], O_RDONLY)) < 0) {
+      printf(2, "split: cannot open %s\n", argv[i]);
       exit();
     }
-    wc(fd, argv[i]);
+    split(fd, argv[i]);
     close(fd);
   }
   exit();
